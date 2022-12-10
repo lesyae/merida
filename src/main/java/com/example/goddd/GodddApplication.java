c97c383c24f9c88b6ce8865133a10f34849ca5d1
lesya <lesyadautova2006@gmail.com>
2022-12-10 15:56:59 +0200

    изменение файлов: 1. index html

---
+++ src/main/java/com/example/goddd/GodddApplication.java
@@ -0,0 +1,13 @@
+package com.example.goddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goddd/GodddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
